48fe044748a85c85bcc3b0f3e123ccee86352664
rgxx <revengexxreturns@gmail.com>
2019-05-10 02:29:53 -0600

    Initiated JavaScript

---
+++ resources/javascript/Script.js
@@ -0,0 +1,20 @@
+var typed = new Typed(".typewriter", {
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1000,
+    backDelay: 1000,
+    startDelay: 0,
+
+    strings: [" Amarjith", " a designer", " a developer"],
+    loop: true,
+    smartBackspace: true,
+    showCursor: false
+
+
+})
+
+
+
+
+
+
